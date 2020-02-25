a712e16e39a6b5b60503a9dd10c900b5ad70f696
aartikansal <aartilumb@gmail.com>
2020-02-25 17:38:34 -0500

    new changes made

--- src/main/java/io/zipcoder/casino/CasinoClasses/Casino.java
+++ src/main/java/io/zipcoder/casino/CasinoClasses/Casino.java
@@ -1,6 +1,8 @@
 package io.zipcoder.casino.CasinoClasses;
 
 
+import io.zipcoder.casino.Player.Player;
+
 import java.util.HashMap;
 
 public class Casino {
@@ -14,6 +16,17 @@ public class Casino {
     public static void main(String[] args) {
         // write your tests before you start fucking with this
     }
+    public Integer checkPlayerAge(Player player){
+        return null;
+    }
+        public Game chooseGame( string )
+
+
+
+
+
+
+
 
 
 }
--- src/main/java/io/zipcoder/casino/Player/BlackJackPlayer.java
+++ src/main/java/io/zipcoder/casino/Player/BlackJackPlayer.java
@@ -1,5 +1,9 @@
 package io.zipcoder.casino.Player;
 
 public class BlackJackPlayer extends Player implements GamblerAttributes {
-    
+
+    public Integer placeBet() {
+        return null;
+    }
 }
+
