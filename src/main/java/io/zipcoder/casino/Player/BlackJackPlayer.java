ee7ea5f0668f643ba2afa9d783c5e62e44c33d46
aartikansal <aartilumb@gmail.com>
2020-02-25 17:20:08 -0500

    chankes made to blackjackplayer

--- src/main/java/io/zipcoder/casino/Player/BlackJackPlayer.java
+++ src/main/java/io/zipcoder/casino/Player/BlackJackPlayer.java
@@ -1,4 +1,5 @@
 package io.zipcoder.casino.Player;
 
-public class BlackJackPlayer {
+public class BlackJackPlayer extends Player implements GamblerAttributes {
+    
 }
--- src/main/java/io/zipcoder/casino/Player/Dealer.java
+++ src/main/java/io/zipcoder/casino/Player/Dealer.java
@@ -1,4 +1,5 @@
 package io.zipcoder.casino.Player;
 
 public class Dealer {
+
 }
