27901a88f0a23c005ebbc81aef81210bd7e712b7
Jeremy McCray <jmccray@zipcodes-MacBook-Pro-2.local>
2020-02-26 11:40:30 -0500

    added some logic to hand

--- src/main/java/io/zipcoder/casino/CasinoClasses/Storage.java
+++ src/main/java/io/zipcoder/casino/CasinoClasses/Storage.java
@@ -1,4 +1,5 @@
 package io.zipcoder.casino.CasinoClasses;
 
 public class Storage {
+
 }
---
+++ src/test/java/io/zipcoder/casino/Player/PlayerTest.java
@@ -0,0 +1,16 @@
+package io.zipcoder.casino.Player;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PlayerTest {
+
+    @Test
+    public void win() {
+    }
+
+    @Test
+    public void lose() {
+    }
+}
