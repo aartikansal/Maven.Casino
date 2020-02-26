2ee49cf3be212917bf6a715bc58bf2543bb2739d
DavidKellyonGitHub <dave.kelly.purchase@gmail.com>
2020-02-26 16:54:32 -0500

    added Games abstract class

--- src/main/java/io/zipcoder/casino/GameClasses/DiceGames/OU7.java
+++ src/main/java/io/zipcoder/casino/GameClasses/DiceGames/OU7.java
@@ -4,15 +4,12 @@ package io.zipcoder.casino.GameClasses.DiceGames;
 import io.zipcoder.casino.CasinoClasses.ConsoleInput;
 import io.zipcoder.casino.CasinoClasses.ConsoleOutput;
 import io.zipcoder.casino.GameClasses.Game;
+import io.zipcoder.casino.GameClasses.Games;
 import io.zipcoder.casino.Player.Player;
 
-public class OU7  implements Game, ConsoleInput, ConsoleOutput {
+public class OU7 extends Games implements ConsoleInput, ConsoleOutput {
 
     Dice pairOfDice = new  Dice(2);
-
-    public void startGame() {
-
-    }
 
     public Boolean quitGame() {
 
--- src/main/java/io/zipcoder/casino/GameClasses/Game.java
+++ src/main/java/io/zipcoder/casino/GameClasses/Game.java
@@ -2,9 +2,11 @@ package io.zipcoder.casino.GameClasses;
 
 import io.zipcoder.casino.Player.Player;
 
+import java.util.ArrayList;
+
 public interface Game {
 
-    public void startGame();
+
 
     public Boolean quitGame();
 
